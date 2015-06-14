90f65e89d0828f8e2c39b3cc06ca9bb798998e4c
yasuaki.ohama <yasuaki.ohama@gmail.com>
2015-06-14 10:16:33 +0900

    modified

--- Assets/NameCreator/Script/MyResourcesPath.cs
+++ Assets/NameCreator/Script/MyResourcesPath.cs
@@ -4,6 +4,6 @@ using System.Collections;
 public static class MyResourcesPath
 {
     public const string SPRITE = "Sprites";
-	public const string BGM = "Sounds/BGM";
-	public const string SE = "Sounds/SE";
+    public const string BGM = "Sounds/BGM";
+    public const string SE = "Sounds/SE";
 }
